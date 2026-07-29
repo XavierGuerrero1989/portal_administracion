b1f693ff0003b428d26c023e4b0871df64b544cb
Xavier Ignacio Guerrero <xavierignacioguerrero@MacBook-Air-de-Xavier.local>
2026-07-29 10:23:27 -0300

    Fix loader rendering with Vite

--- src/componentes/Loader.jsx
+++ src/componentes/Loader.jsx
@@ -2,9 +2,13 @@
 
 import "./Loader.scss";
 
-import Lottie from "lottie-react";
+import LottieModule from "lottie-react";
 import React from "react";
 import animationData from "../assets/Loader.json";
+
+// lottie-react is published as CommonJS. With Vite 8 its default export can
+// arrive wrapped one extra time, so normalize it before rendering.
+const Lottie = LottieModule.default ?? LottieModule;
 
 const Loader = () => {
   return (
